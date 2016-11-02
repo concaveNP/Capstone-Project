5f6569a751b1f931f04bde446676fedec1213ccb
Dave Todd <concave@acm.org>
2016-11-01 21:16:26 -0600

    Firebase libraries started and Google server has app SHA1.

---
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/model/ArtProject.java
@@ -0,0 +1,20 @@
+package com.concavenp.artistrymuse.model;
+
+import java.util.Date;
+
+/**
+ * Created by dave on 10/14/2016.
+ */
+
+public class ArtProject {
+
+    public String uid;
+    public String username;
+    public String name;
+    public String description;
+    public Date creationDate;
+//    public
+//
+//    how to store images?:????? UID is a string, is UID right term for "Storage" interaction
+
+}
---
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/model/User.java
@@ -0,0 +1,8 @@
+package com.concavenp.artistrymuse.model;
+
+/**
+ * Created by dave on 10/14/2016.
+ */
+
+public class User {
+}
