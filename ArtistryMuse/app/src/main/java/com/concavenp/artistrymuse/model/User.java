3320d7be5d52cfd41f8bcdb888cb8c11803631b8
Dave Todd <concave@acm.org>
2016-11-03 12:59:05 -0600

    Created very simple POJOs with no real comments or methods within them.

--- ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/model/User.java
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/model/User.java
@@ -1,8 +1,31 @@
 package com.concavenp.artistrymuse.model;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by dave on 10/14/2016.
  */
 
+/**
+ * This class represents the the data model of a "User" object.  This data will be stored within
+ * the DB
+ */
 public class User {
+
+    public String uid;
+    public String username;
+    public String name;
+    public String description;
+    public Date creationDate;
+    public Date lastUpdateDate;
+    public String avatarImageUid;
+    public List<String> artProjects;
+    public List<Favorite> favorites;
+    public List<Following> followings;
+
+    public User() {
+        // TODO: comment fix - Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
 }
