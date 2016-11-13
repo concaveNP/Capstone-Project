e5a818b8d443aa266322f168c8aed31ff742b3a6
Dave Todd <concave@acm.org>
2016-11-12 22:13:08 -0700

    Have the tabs and fragments setup and working.

--- ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/MainActivity.java
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/MainActivity.java
@@ -1,17 +1,38 @@
 package com.concavenp.artistrymuse;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        FollowingFragment.OnFragmentInteractionListener,
+        FavoritesFragment.OnFragmentInteractionListener,
+        SearchFragment.OnFragmentInteractionListener,
+        GalleryFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        // Force the scroll view to fill the area, dunno why this is not the default.
+        NestedScrollView scrollView = (NestedScrollView) findViewById (R.id.nest_scrollview);
+        scrollView.setFillViewport (true);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new ArtistryFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+       // TODO: determine if this needs to be here, this is used by all of the fragments for interaction.
     }
 }
