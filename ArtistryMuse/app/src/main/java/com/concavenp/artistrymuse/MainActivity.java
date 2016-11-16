c2587e0a28551416767db07d2ea074b8b0b1b784
Dave Todd <concave@acm.org>
2016-11-15 22:32:01 -0700

    Tried to get the settings work, but gave up for now.  Basically punting on the rest of the "get the user flow working" aspect in order to keep on trucking.

--- ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/MainActivity.java
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/MainActivity.java
@@ -1,11 +1,16 @@
 package com.concavenp.artistrymuse;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.concavenp.artistrymuse.fragments.ArtistryFragmentPagerAdapter;
 import com.concavenp.artistrymuse.fragments.FavoritesFragment;
@@ -24,6 +29,12 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // If the application has not run before then initialize the preference settings with default values
+        if (savedInstanceState == null) {
+            // These are the "general" preferences (its all this app has)
+            PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        }
+
         // Force the scroll view to fill the area, dunno why this is not the default.
         NestedScrollView scrollView = (NestedScrollView) findViewById (R.id.nest_scrollview);
         scrollView.setFillViewport (true);
@@ -41,4 +52,39 @@ public class MainActivity extends AppCompatActivity implements
     public void onFragmentInteraction(Uri uri) {
        // TODO: determine if this needs to be here, this is used by all of the fragments for interaction.
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        boolean result = false;
+
+        switch (item.getItemId()) {
+
+            case R.id.action_settings:
+
+                // User chose the "Settings" item, show the app settings UI...
+
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+
+                result = true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                result = super.onOptionsItemSelected(item);
+
+        }
+
+        return result;
+
+    }
+
 }
