dcc52c744a5b3e98da3cebf2becb3838815ff3b2
Dave Todd <concave@acm.org>
2016-11-29 12:42:53 -0700

    Not perfect, but files are now being uploaded to a user specific directory.

--- ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/fragments/FollowingFragment.java
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/fragments/FollowingFragment.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.concavenp.artistrymuse.ProjectActivity;
 import com.concavenp.artistrymuse.R;
 
 import java.io.File;
@@ -40,12 +41,7 @@ public class FollowingFragment extends Fragment {
 
     private static final String TAG = "FollowingFragment";
 
-    // TODO: probably delete these later
-    private static final int RC_TAKE_PICTURE = 101;
-    private static final int RC_STORAGE_PERMS = 102;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // TODO: Rename parameter arguments, choose names that match the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -54,8 +50,6 @@ public class FollowingFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-
-    private Uri mFileUri = null;
 
     public FollowingFragment() {
         // Required empty public constructor
@@ -94,73 +88,16 @@ public class FollowingFragment extends Fragment {
         // Inflate the layout for this fragment
         View mainView = inflater.inflate(R.layout.fragment_following, container, false);
 
-        Button uploadButton = (Button) mainView.findViewById(R.id.button_upload);
+        Button uploadButton = (Button) mainView.findViewById(R.id.start_project_activity);
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchCamera();
+                Intent intent = new Intent(getActivity(), ProjectActivity.class);
+                startActivity(intent);
             }
         });
 
         return mainView;
-    }
-
-    @AfterPermissionGranted(RC_STORAGE_PERMS)
-    private void launchCamera() {
-
-        Log.d(TAG, "launchCamera");
-
-        // TODO: update the message for the rationale
-
-        // Check that we have permission to read images from external storage.
-        String perm = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-        if (!EasyPermissions.hasPermissions(getContext(), perm)) {
-            EasyPermissions.requestPermissions(this, getString(R.string.rationale_storage),
-                    RC_STORAGE_PERMS, perm);
-            return;
-        }
-
-        // Choose file storage location, must be listed in res/xml/file_paths.xml
-        File dir = new File(Environment.getExternalStorageDirectory() + "/photos");
-        File file = new File(dir, UUID.randomUUID().toString() + ".jpg");
-        try {
-            // Create directory if it does not exist.
-            if (!dir.exists()) {
-
-                boolean result = dir.mkdir();
-
-                if (result) {
-                    Log.d(TAG, "Directory was created" );
-                } else {
-                    Log.d(TAG, "Directory was NOT created" );
-                }
-
-            }
-            boolean created = file.createNewFile();
-            Log.d(TAG, "file.createNewFile:" + file.getAbsolutePath() + ":" + created);
-        } catch (IOException e) {
-            Log.e(TAG, "file.createNewFile" + file.getAbsolutePath() + ":FAILED", e);
-        } catch (SecurityException e) {
-            Log.e(TAG, "file.createNewFile" + file.getAbsolutePath() + ":FAILED", e);
-        }
-
-        // Create content:// URI for file, required since Android N
-        // See: https://developer.android.com/reference/android/support/v4/content/FileProvider.html
-        mFileUri = FileProvider.getUriForFile(getContext(), "com.concavenp.artistrymuse.fileprovider", file);
-
-        // Create and launch the intent
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mFileUri);
-
-        // Grant permission to camera (this is required on KitKat and below)
-        List<ResolveInfo> resolveInfos = getContext().getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
-        for (ResolveInfo resolveInfo : resolveInfos) {
-            String packageName = resolveInfo.activityInfo.packageName;
-            getContext().grantUriPermission(packageName, mFileUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        }
-
-        // Start picture-taking intent
-        startActivityForResult(takePictureIntent, RC_TAKE_PICTURE);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
