e7e39ff278863d367b65436169b82379ec93ac09
Dave Todd <concave@acm.org>
2016-12-15 00:17:27 -0700

    Little more layout work completed.

--- ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/fragments/viewholder/FollowingViewHolder.java
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/fragments/viewholder/FollowingViewHolder.java
@@ -2,7 +2,6 @@ package com.concavenp.artistrymuse.fragments.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,8 +21,6 @@ import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.util.Date;
-
 /**
  * Created by dave on 12/2/2016.
  */
@@ -36,17 +33,18 @@ public class FollowingViewHolder extends RecyclerView.ViewHolder {
     private FirebaseUser mUser;
     private String mUid;
 
-    public TextView uidTextView;
-    public EditText lastUpdateEditText;
-    public ImageView thumbnailImage;
+    public ImageView headerImageView;
+    public ImageView profileImageView;
+    public TextView usernameTextView;
+    public TextView summaryTextView;
+    public TextView descriptionTextView;
+    public TextView followedTextView;
+    public TextView followingTextView;
 
     public FollowingViewHolder(View itemView) {
 
         super(itemView);
 
-        uidTextView = (TextView) itemView.findViewById(R.id.uid);
-        lastUpdateEditText = (EditText) itemView.findViewById(R.id.last_update);
-        thumbnailImage = (ImageView) itemView.findViewById(R.id.thumbnail);
 
         // Initialize the Database connection
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -64,43 +62,37 @@ public class FollowingViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bindToPost(Following following, View.OnClickListener clickListener) {
-
-        uidTextView.setText(following.uid);
-        System.out.println("Following UI: " + following.uid);
-        lastUpdateEditText.setText(new Date(following.lastUpdatedDate).toString());
-
-        uidTextView.setOnClickListener(clickListener);
+        // Display items to be populated
+        headerImageView = (ImageView) itemView.findViewById(R.id.header_imageview);
+        profileImageView = (ImageView) itemView.findViewById(R.id.profile_imageview);
+        usernameTextView = (TextView) itemView.findViewById(R.id.username_textview);
+        summaryTextView = (TextView) itemView.findViewById(R.id.summary_textview);
+        descriptionTextView = (TextView) itemView.findViewById(R.id.description_textview);
+        followedTextView = (TextView) itemView.findViewById(R.id.followed_textview);
+        followingTextView = (TextView) itemView.findViewById(R.id.following_textview);
 
         mDatabase.child("users").child(following.uid).addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                User user = (User) dataSnapshot.getValue(User.class);
+                // Perform the JSON to Object conversion
+                User user = dataSnapshot.getValue(User.class);
 
                 // TODO: what to do when it is null
+
                 // Verify there is a user to work with
                 if (user != null) {
 
-                    String profileUid = user.profileImageUid;
-                    if ((profileUid  != null) && (!profileUid.isEmpty())) {
+                    populateImageView(user.uid, user.headerImageUid, headerImageView);
+                    populateImageView(user.uid, user.profileImageUid, profileImageView);
+                    populateTextView(user.username, usernameTextView);
+                    populateTextView(user.summary, summaryTextView);
+                    populateTextView(user.description, descriptionTextView);
+                    populateTextView(Integer.toString(user.followedCount), followedTextView);
+                    populateTextView(Integer.toString(user.following.size()), followingTextView);
 
-                        final String fileReference = "users" + "/" + user.uid + "/" + user.profileImageUid + ".jpg";
-                        StorageReference storageReference = mStorageRef.child(fileReference);
-
-                        System.out.println(storageReference);
-
-                        // Download directly from StorageReference using Glide
-                        Glide.with(thumbnailImage.getContext())
-                                .using(new FirebaseImageLoader())
-                                .load(storageReference)
-                                .centerCrop()
-                                .crossFade()
-                                .into(thumbnailImage);
-
-                    }
                 }
-
-                System.out.println(user);
 
             }
 
@@ -108,10 +100,42 @@ public class FollowingViewHolder extends RecyclerView.ViewHolder {
             public void onCancelled(DatabaseError databaseError) {
 
             }
+
         });
 
+    }
 
+    private void populateImageView(String uid, String imageUid, ImageView imageView) {
+        if ((imageUid != null) && (!imageUid.isEmpty())) {
+
+            final String fileReference = "users" + "/" + uid + "/" + imageUid + ".jpg";
+            StorageReference storageReference = mStorageRef.child(fileReference);
+
+            // Download directly from StorageReference using Glide
+            Glide.with(imageView.getContext())
+                    .using(new FirebaseImageLoader())
+                    .load(storageReference)
+                    .fitCenter()
+                    .crossFade()
+                    .into(imageView);
+
+        }
+    }
+
+    private void populateTextView(String text, TextView textView) {
+
+        // Verify there is text to work with and empty out if nothing is there.
+        if ((text != null) && (!text.isEmpty())) {
+
+            textView.setText(text);
+
+        } else {
+
+            textView.setText("");
+
+        }
 
     }
 
 }
+
