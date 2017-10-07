8584424495a85381bfb7b19948f24ad3879f2fde
Dave Todd <concave@acm.org>
2017-10-07 17:21:01 -0600

    Updated the copyright notice

--- ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/ProjectEditActivity.java
+++ ArtistryMuse/app/src/main/java/com/concavenp/artistrymuse/ProjectEditActivity.java
@@ -1,3 +1,23 @@
+/*
+ * ArtistryMuse is an application that allows artist to share projects
+ * they have created along with the inspirations behind them for others to
+ * discover and enjoy.
+ * Copyright (C) 2017  David A. Todd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.concavenp.artistrymuse;
 
 import android.content.Intent;
@@ -23,7 +43,6 @@ import com.concavenp.artistrymuse.model.Inspiration;
 import com.concavenp.artistrymuse.model.Project;
 import com.concavenp.artistrymuse.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.database.DataSnapshot;
